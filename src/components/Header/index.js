1275abeb3153ed98cadb8cdfa949a83eb9d57b96
Manoj Mantha <manojputtaluckey@gmail.com>
2023-06-27 12:19:04 +0000

    Authentication

---
+++ src/components/Header/index.js
@@ -0,0 +1,50 @@
+import {Link, withRouter} from 'react-router-dom'
+import './index.css'
+import Cookies from 'js-cookie'
+
+const Header = props => {
+  const onLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+  return (
+    <nav className="nav-header">
+      <div className="nav-content">
+        <img
+          className="website-logo"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          alt="website logo"
+        />
+        <ul className="nav-menu">
+          <li>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/products" className="nav-link">
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart" className="nav-link">
+              Cart
+            </Link>
+          </li>
+        </ul>
+        <button type="button" onClick={onLogout} className="logout-desktop-btn">
+          Logout
+        </button>
+        <button type="button" className="logout-mobile-btn">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+            alt="logout icon"
+            className="logout-icon"
+          />
+        </button>
+      </div>
+    </nav>
+  )
+}
+export default withRouter(Header)
